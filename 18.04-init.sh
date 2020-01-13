ae5ac9c6530601a49fa2314bd577928c4934c695
张星 <1153673799@qq.com>
2020-01-13 15:13:13 +0800

    Update 18.04-init.sh

--- 18.04-init.sh
+++ 18.04-init.sh
@@ -4,7 +4,7 @@ sed -i 's/http:\/\/archive.ubuntu.com\/ubuntu/http:\/\/mirrors.163.com\/ubuntu/g
 sed -i 's/http:\/\/security.ubuntu.com\/ubuntu/http:\/\/mirrors.163.com\/ubuntu/g' /etc/apt/sources.list
 
 sudo apt install zsh
-sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+# sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 chsh -s $(which zsh)
 sudo apt update && apt -y upgrade && apt install -y php-curl php-fpm php-mbstring php-mysql php-xml php-zip php-gd php-dev php-pear php-bcmath composer nginx redis mysql-server libpng-dev
 sudo apt-get install \
@@ -19,3 +19,5 @@ sudo add-apt-repository \
      stable"
 sudo apt-get update
 sudo apt install docker.io
+sudo usermod -aG docker $USER
+sudo cgroupfs-mount
